914f4782ede8612ad149f0d4de233c7ba648ee56
Sheikh Saif Zaman <sheikhsaifzaman54@gmail.com>
2021-04-02 21:26:16 +0600

    done

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const booksCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION}`);
-  
+  const ordersCollection = client.db(`${process.env.DB_NAME}`).collection(`orders`);
 
   app.post('/addBook',(req,res)=>{
     const newBookInfo=req.body;
@@ -32,6 +32,39 @@ client.connect(err => {
     booksCollection.find({})
     .toArray((error,documents)=>{
       res.send(documents);
+    })
+  })
+
+  app.delete('/DeleteOne/:id',(req,res)=>{
+    booksCollection.deleteOne({_id:ObjectID(req.params.id)})
+    .then(result=>{
+      console.log(result);
+      res.send(result.deletedCount>0)
+    })
+  })
+
+  app.post('/placeOrder',(req,res)=>{
+    const newOrder=req.body;
+    ordersCollection.insertOne(newOrder)
+    .then(result=>{
+      if(result.insertedCount>0){
+        console.log("INSERTED");
+      }
+    })
+  })
+
+  app.get('/getOrders',(req,res)=>{
+    const getOrders=req.query.email;
+    let total=0;
+    ordersCollection.find({userMail:getOrders})
+    .toArray((error,documents)=>{
+      for (let i = 0; i < documents.length; i++) {
+        const elementPrice = parseInt(documents[i].bookPrice);
+        total=total+elementPrice;
+      }
+      documents.push(total);
+      res.send(documents);
+      console.log(documents,total);
     })
   })
 });
