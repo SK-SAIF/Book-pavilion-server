2e2fc50d217ab6056871edf9c63785da30dabaac
Sheikh Saif Zaman <sheikhsaifzaman54@gmail.com>
2021-04-05 22:56:49 +0600

    Added a Procfile.

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 5000;
 
-const uri = `mongodb+srv://bookUser:dJKGNKYIpw8WOzcF@cluster0.wkh8y.mongodb.net/BookPavilion?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wkh8y.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const booksCollection = client.db(`BookPavilion`).collection(`Books`);
-  const ordersCollection = client.db(`BookPavilion`).collection(`orders`);
+  const booksCollection = client.db(`${process.env.DB_NAME}`).collection(`Books`);
+  const ordersCollection = client.db(`${process.env.DB_NAME}`).collection(`orders`);
 
   app.post('/addBook',(req,res)=>{
     const newBookInfo=req.body;
